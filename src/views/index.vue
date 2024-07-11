6505933d6721ec880959f4db6214598a96f6fc4e
Nebula <jing.me@icloud.com>
2024-07-11 14:46:00 +0800

    feat: 修改首页

--- src/views/index.vue
+++ src/views/index.vue
@@ -5,7 +5,7 @@ import welcomeImg from '@/assets/images/welcome.jpg'
 <template>
   <div class="home">
     <el-image :src="welcomeImg" style="width: 100%" fit="fill" />
-    <h1>欢迎使用系统</h1>
+    <h1>欢迎使用管理系统</h1>
   </div>
 </template>
 
@@ -20,15 +20,14 @@ import welcomeImg from '@/assets/images/welcome.jpg'
   h1 {
     position: absolute;
     top: 20px;
-    left: 42%;
+    left: 41%;
     color: #fff;
-    font-size: 34px;
-    font-weight: 600;
-    text-align: center;
-    width: 300px;
-    height: 50px;
-    line-height: 50px;
-    background: rgba(0, 0, 0, 0.7);
+    text-shadow: 0 0 20px #000;
+    letter-spacing: 2px;
+    font-family: "楷体";
+    font-weight: bold;
+    font-size: 36px;
+    line-height: 40px;
   }
 }
 </style>
